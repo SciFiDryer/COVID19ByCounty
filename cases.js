83404dca14d7fb696a1ee4dcaea25cb6b43a4750
SciFiDryer <54369529+SciFiDryer@users.noreply.github.com>
2020-03-18 17:11:30 -0400

    Add files via upload
    added javascript

---
+++ cases.js
@@ -0,0 +1,101 @@
+var countycases = { "counties" : [
+{"name": "Albany", "stateid": 36,"cases": 36}
+,
+{"name": "Allegany", "stateid": 36,"cases": 2}
+,
+{"name": "Broome", "stateid": 36,"cases": 1}
+,
+{"name": "Chenango", "stateid": 36,"cases": 1}
+,
+{"name": "Clinton", "stateid": 36,"cases": 1}
+,
+{"name": "Delaware", "stateid": 36,"cases": 1}
+,
+{"name": "Dutchess", "stateid": 36,"cases": 20}
+,
+{"name": "Erie", "stateid": 36,"cases": 7}
+,
+{"name": "Essex", "stateid": 36,"cases": 1}
+,
+{"name": "Greene", "stateid": 36,"cases": 2}
+,
+{"name": "Hamilton", "stateid": 36,"cases": 1}
+,
+{"name": "Herkimer", "stateid": 36,"cases": 1}
+,
+{"name": "Monroe", "stateid": 36,"cases": 14}
+,
+{"name": "Montgomery", "stateid": 36,"cases": 2}
+,
+{"name": "Nassau", "stateid": 36,"cases": 183}
+,
+{"name": "New York City", "stateid": 36,"cases": 1339}
+,
+{"name": "Onondaga", "stateid": 36,"cases": 2}
+,
+{"name": "Ontario", "stateid": 36,"cases": 1}
+,
+{"name": "Orange", "stateid": 36,"cases": 32}
+,
+{"name": "Putnam", "stateid": 36,"cases": 2}
+,
+{"name": "Rensselaer", "stateid": 36,"cases": 4}
+,
+{"name": "Rockland", "stateid": 36,"cases": 30}
+,
+{"name": "Saratoga", "stateid": 36,"cases": 14}
+,
+{"name": "Schenectady", "stateid": 36,"cases": 14}
+,
+{"name": "Suffolk", "stateid": 36,"cases": 116}
+,
+{"name": "Sullivan", "stateid": 36,"cases": 1}
+,
+{"name": "Tioga", "stateid": 36,"cases": 1}
+,
+{"name": "Tompkins", "stateid": 36,"cases": 3}
+,
+{"name": "Ulster", "stateid": 36,"cases": 9}
+,
+{"name": "Warren", "stateid": 36,"cases": 1}
+,
+{"name": "Washington", "stateid": 36,"cases": 1}
+,
+{"name": "Westchester", "stateid": 36,"cases": 538}
+,
+{"name": "Wyoming", "stateid": 36,"cases": 1}
+,
+{"name": "Total Number of Positive Cases", "stateid": 36, "cases": 2382}
+,
+{"name": "Allegheny", "stateid": 42, "cases": 11}
+,
+{"name": "Beaver", "stateid": 42, "cases": 2}
+,
+{"name": "Bucks", "stateid": 42, "cases": 9}
+,
+{"name": "Chester", "stateid": 42, "cases": 9}
+,
+{"name": "Cumberland", "stateid": 42, "cases": 10}
+,
+{"name": "Delaware", "stateid": 42, "cases": 14}
+
+,
+{"name": "Lehigh", "stateid": 42, "cases": 1}
+,
+{"name": "Luzerne", "stateid": 42, "cases": 1}
+,
+{"name": "Monroe", "stateid": 42, "cases": 7}
+,
+{"name": "Montgomery", "stateid": 42, "cases": 42}
+,
+{"name": "Northampton", "stateid": 42, "cases": 1}
+,
+{"name": "Philadelphia", "stateid": 42, "cases": 17}
+,
+{"name": "Pike", "stateid": 42, "cases": 2}
+,
+{"name": "Washington", "stateid": 42, "cases": 2}
+,
+{"name": "Wayne", "stateid": 42, "cases": 1}
+
+]}
